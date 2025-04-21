5aac1622229f0fabc2e62332fc69f188db188b63
Aatreyee Mukherjee <aatreyee.mukherjee@ibm.com>
2025-04-21 23:52:44 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
 locals {
-  account_id                        = var.existing_watsonx_orchestrate_instance_crn != null ? module.crn_parser[0].account_id : ibm_resource_instance.watsonx_orchestrate_instance[0].account_id
-  watsonx_orchestrate_id            = var.existing_watsonx_orchestrate_instance_crn != null ? data.ibm_resource_instance.existing_orchestrate_instance[0].id : ibm_resource_instance.watsonx_orchestrate_instance[0].id
-  watsonx_orchestrate_crn           = var.existing_watsonx_orchestrate_instance_crn != null ? data.ibm_resource_instance.existing_orchestrate_instance[0].crn : ibm_resource_instance.watsonx_orchestrate_instance[0].crn
-  watsonx_orchestrate_guid          = var.existing_watsonx_orchestrate_instance_crn != null ? data.ibm_resource_instance.existing_orchestrate_instance[0].guid : ibm_resource_instance.watsonx_orchestrate_instance[0].guid
-  watsonx_orchestrate_name          = var.existing_watsonx_orchestrate_instance_crn != null ? data.ibm_resource_instance.existing_orchestrate_instance[0].resource_name : ibm_resource_instance.watsonx_orchestrate_instance[0].resource_name
-  watsonx_orchestrate_plan_id       = var.existing_watsonx_orchestrate_instance_crn != null ? null : ibm_resource_instance.watsonx_orchestrate_instance[0].resource_plan_id
-  watsonx_orchestrate_dashboard_url = var.existing_watsonx_orchestrate_instance_crn != null ? null : ibm_resource_instance.watsonx_orchestrate_instance[0].dashboard_url
+  account_id                  = var.existing_watsonx_orchestrate_instance_crn != null ? module.crn_parser[0].account_id : ibm_resource_instance.watsonx_orchestrate_instance[0].account_id
+  watsonx_orchestrate_id      = var.existing_watsonx_orchestrate_instance_crn != null ? data.ibm_resource_instance.existing_orchestrate_instance[0].id : ibm_resource_instance.watsonx_orchestrate_instance[0].id
+  watsonx_orchestrate_crn     = var.existing_watsonx_orchestrate_instance_crn != null ? data.ibm_resource_instance.existing_orchestrate_instance[0].crn : ibm_resource_instance.watsonx_orchestrate_instance[0].crn
+  watsonx_orchestrate_guid    = var.existing_watsonx_orchestrate_instance_crn != null ? data.ibm_resource_instance.existing_orchestrate_instance[0].guid : ibm_resource_instance.watsonx_orchestrate_instance[0].guid
+  watsonx_orchestrate_name    = var.existing_watsonx_orchestrate_instance_crn != null ? data.ibm_resource_instance.existing_orchestrate_instance[0].resource_name : ibm_resource_instance.watsonx_orchestrate_instance[0].resource_name
+  watsonx_orchestrate_plan_id = var.existing_watsonx_orchestrate_instance_crn != null ? null : ibm_resource_instance.watsonx_orchestrate_instance[0].resource_plan_id
+  # watsonx_orchestrate_dashboard_url = var.existing_watsonx_orchestrate_instance_crn != null ? null : "https://cloud.ibm.com/services/watsonx-orchestrate/${urlencode(local.watsonx_orchestrate_crn)}?paneId=manage&new=true"
+  watsonx_orchestrate_dashboard_url = local.watsonx_orchestrate_crn != null ? "https://us-south.watson-orchestrate.cloud.ibm.com/home/ws?plan_id=${urlencode(local.watsonx_orchestrate_plan_id)}" : null
+
 }
 
 module "crn_parser" {
