5d5c172c4b4df98fb0c280f4bd09597af0fa4d8b
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2025-05-16 14:16:12 +0530

    fix: added migration changes

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -28,7 +28,19 @@ variable "existing_resource_group_name" {
 variable "prefix" {
   type        = string
   nullable    = true
-  description = "The prefix to add to all resources that this solution creates (e.g `prod`, `test`, `dev`). To not use any prefix value, you can set this value to `null` or an empty string."
+  description = "The prefix to be added to all resources created by this solution. To skip using a prefix, set this value to null or an empty string. The prefix must begin with a lowercase letter and may contain only lowercase letters, digits and hyphens ('-'). It should not exceed 16 characters, must not end with a hyphen ('-'), and can not contain consecutive hyphens ('--'). Example: wx-1605-orch.[Learn more](https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone-vpc/blob/issue_13328/solutions/fully-configurable/DA-prefix.md)"
+
+  validation {
+    condition = var.prefix == null || var.prefix == "" ? true : alltrue([
+      can(regex("^[a-z][-a-z0-9]*[a-z0-9]$", var.prefix)), length(regexall("--", var.prefix)) == 0
+    ])
+    error_message = "Prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens ('-'). It must not end with a hyphen ('-'), and cannot contain consecutive hyphens ('--')."
+  }
+
+  validation {
+    condition     = var.prefix == null || var.prefix == "" ? true : length(var.prefix) <= 16
+    error_message = "Prefix must not exceed 16 characters."
+  }
 }
 
 variable "watsonx_orchestrate_instance_name" {
