bcf8e8d8f93a5e0a06a28b43730e45d4073f4127
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2025-05-26 09:42:49 +0530

    fix: removed link

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -29,7 +29,7 @@ variable "existing_resource_group_name" {
 variable "prefix" {
   type        = string
   nullable    = true
-  description = "The prefix to be added to all resources created by this solution. To skip using a prefix, set this value to null or an empty string. The prefix must begin with a lowercase letter and may contain only lowercase letters, digits and hyphens ('-'). It should not exceed 16 characters, must not end with a hyphen ('-'), and can not contain consecutive hyphens ('--'). Example: wx-1605-orch.[Learn more](https://terraform-ibm-modules.github.io/documentation/#/da-implementation-guidelines.md)"
+  description = "The prefix to be added to all resources created by this solution. To skip using a prefix, set this value to null or an empty string. The prefix must begin with a lowercase letter and may contain only lowercase letters, digits and hyphens ('-'). It should not exceed 16 characters, must not end with a hyphen ('-'), and can not contain consecutive hyphens ('--'). Example: wx-1605-orch."
 
   validation {
     condition = var.prefix == null || var.prefix == "" ? true : alltrue([
