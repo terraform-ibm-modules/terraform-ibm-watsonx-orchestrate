7a41458ac7691a519a33b0bc39d64dd0af79ea7b
zikra-iqbal <zikra.iqbalmnnit@gmail.com>
2025-12-11 10:45:25 +0530

    feat: Added the next-step URL for watsonx-orchestratet

--- solutions/fully-configurable/outputs.tf
+++ solutions/fully-configurable/outputs.tf
@@ -31,3 +31,28 @@ output "dashboard_url" {
   description = "The dashboard URL of the watsonx Orchestrate instance."
   value       = module.watsonx_orchestrate.dashboard_url
 }
+
+output "next_steps_text" {
+  value       = "Now, you can launch the Watsonx Orchestrate tool and create your agents."
+  description = "Next steps text"
+}
+
+output "next_step_primary_label" {
+  value       = "Go to Watsonx Orchestrate instance dashboard"
+  description = "Primary label"
+}
+
+output "next_step_primary_url" {
+  value       = "${module.watsonx_orchestrate.dashboard_url}?paneId=manage"
+  description = "Primary URL"
+}
+
+output "next_step_secondary_label" {
+  value       = "Learn more about Watsonx Orchestrate"
+  description = "Secondary label"
+}
+
+output "next_step_secondary_url" {
+  value       = "https://www.ibm.com/docs/en/watsonx/watson-orchestrate/base?topic=getting-started-watsonx-orchestrate"
+  description = "Secondary URL"
+}
